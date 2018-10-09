0b9ab227200e41f0e5e0e4d85d23f129ce851d77
1191179102@qq.com <1191179102@qq.com>
2018-10-09 11:47:33 +0800

    test

---
+++ src/main/Java/com/qiaozhen/beans/Damo2.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2018-2018, XXX有限公司
+ * FileName: Damo2
+ * Author:   Administrator
+ * Date:     2018/10/9 11:45
+ * Description: testGit
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * qiaozhen         修改时间          版本号             描述
+ */
+package com.qiaozhen.beans;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈testGit〉
+ *
+ * @author Administrator
+ * @create 2018/10/9
+ * @since 1.0.0
+ */
+
+public class Damo2 {
+}
